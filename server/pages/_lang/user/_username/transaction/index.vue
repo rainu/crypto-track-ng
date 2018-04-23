12931a010d7bc1566b4d20a3ecbc98cf461d2519
rainu <rainu@raysha.de>
2018-04-23 20:54:50 +0200

    implement transaction deletion functionality

--- server/pages/_lang/user/_username/transaction/index.vue
+++ server/pages/_lang/user/_username/transaction/index.vue
@@ -8,6 +8,13 @@
         <nuxt-link :to="{name: 'lang-user-username-transaction-edit-id', params: { id: tx.id }}" tag="a" role="button" class="btn btn-block btn-primary" :title="$t('common.edit')">
           <i class="fa fa-pencil"></i>
         </nuxt-link>
+
+        <modal-warning v-if="deleteRequest === tx.id" :payload="{ transactionId: tx.id }" @decided="handleDeleteDecision">
+          <span v-html="$t('transaction.delete-question', {
+            date: ldate(tx.date),
+            type: dType(tx.type)
+          })"></span>
+        </modal-warning>
       </template>
     </transaction-table>
     <div class="row">
@@ -23,6 +30,8 @@
 </template>
 
 <script>
+  import moment from 'moment'
+  import { mapActions } from 'vuex'
 
   export default {
     data(){
@@ -32,6 +41,34 @@
     },
     computed: {
     },
+    methods: {
+      ...mapActions({
+        deleteTransaction: 'transaction/deleteTransaction'
+      }),
+      requestDeleteTransaction(id){
+        this.deleteRequest = id
+      },
+      handleDeleteDecision(decision) {
+        this.deleteRequest = null
+
+        if(decision.option === 'ok') {
+          this.deleteTransaction(decision.payload.transactionId)
+        }
+      },
+      ldate(date){
+        return moment(date).format(this.$t('common.datetime.format'))
+      },
+      dType(type){
+        switch(type.toLowerCase()){
+          case 'giftin':
+            return this.$t(`transaction.gift.in`)
+          case 'giftout':
+            return this.$t(`transaction.gift.out`)
+          default:
+            return this.$t(`transaction.${type}.title`)
+        }
+      }
+    }
   }
 </script>
 
